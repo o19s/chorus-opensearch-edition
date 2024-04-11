58acae3550d93b878f91e93d69f720f7ac180256
Eric Pugh <epugh@opensourceconnections.com>
2024-04-11 14:40:45 -0400

    Use a dockerized python env to transform data instead of cryptic shell scripts.

--- quickstart.sh
+++ quickstart.sh
@@ -18,16 +18,8 @@ if ! [ -x "$(command -v docker)" ]; then
   echo "${ERROR}Error: docker is not installed.${RESET}" >&2
   exit 1
 fi
-if ! [ -x "$(command -v jq)" ]; then
-  echo "${ERROR}Error: jq is not installed.${RESET}" >&2
-  exit 1
-fi
 if ! [ -x "$(command -v wget)" ]; then
   echo "${ERROR}Error: wget is not installed.${RESET}" >&2
-  exit 1
-fi
-if ! [ -x "$(command -v python3)" ]; then
-  echo "${ERROR}Error: python3 is not installed.${RESET}" >&2
   exit 1
 fi
 
@@ -119,8 +111,7 @@ fi
 
 if [ ! -f ./transformed_data.json ]; then
   echo -e "${MINOR}Transforming the sample product data into JSON format, please give it a few minutes!\n${RESET}"
-  # if Windows, change to python
-  python3 ./opensearch/transform_data.py icecat-products-w_price-19k-20201127.json transformed_data.json
+  docker run -v ./:/app -w /app python:3 python3 ./opensearch/transform_data.py icecat-products-w_price-19k-20201127.json transformed_data.json
 fi
 echo -e "${MAJOR}Indexing the sample product data, please wait...\n${RESET}"
 curl -s -X POST "http://localhost:9200/ecommerce/_bulk?pretty=false&filter_path=-items" -H 'Content-Type: application/json' --data-binary @transformed_data.json
