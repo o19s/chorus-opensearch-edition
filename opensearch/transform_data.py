2ae55f03ae2ccaf8bb5b46703e72725b8fa1b7dc
RasonJ <145287540+RasonJ@users.noreply.github.com>
2024-05-31 08:32:25 -0700

    change margin to cost

--- opensearch/transform_data.py
+++ opensearch/transform_data.py
@@ -14,9 +14,9 @@ data = json.load(fIn)
 with open(fOut, 'w', encoding='utf8') as fOut:
 	for row in data:
 		price = row.get('price', 1)
-		margin = 0 if price < 2 else random.randint(0, price - 1)
+		cost = 0 if price < 2 else random.randint(0, price - 1)
 
 		row['primary_ean'] = row['ean'][0]
-		row['margin'] = margin
+		row['cost'] = cost
 		fOut.write('{ "index" : {"_id" : "' + row['id'] + '"}}\n')
 		fOut.write(json.dumps(row) + '\n')
