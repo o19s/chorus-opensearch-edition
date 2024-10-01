f4983cbf3dd630bbab4abfcd7a5e542e98ea1d96
Eric Pugh <epugh@opensourceconnections.com>
2024-10-01 16:06:59 -0400

    Add seperate _msearch and _search end points
    This supports now caching the cost.

--- middleware/app.py
+++ middleware/app.py
@@ -1,7 +1,6 @@
 import json
 import os
 import uuid
-
 import flask
 import requests
 from flask import Flask, request, Response
@@ -26,43 +25,107 @@ cache = {}
 
 print("Using OTel endpoint: " + OTEL_COLLECTOR_ENDPOINT)
 
+# This proxies the traditional _search end point across any index.
+# ecommerce/_search
+# ecommerce_keyword/_search
+# ecommerce_neural/_search
+@app.route('/<path:prefix>/_search', methods=["GET", "POST", "OPTIONS"])
+def search(prefix):
 
-@app.route("/ecommerce/_search", methods=["GET"])
-def search():
+    if request.method == "OPTIONS":
+        response = flask.jsonify(status=200, mimetype="application/json")
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Authorization,Content-Type")
+        response.headers.add("Access-Control-Request-Method", "*")
+        return response
 
-    # Based on https://stackoverflow.com/a/36601467
-    res = requests.request(
-        method          = request.method,
-        url             = request.url.replace(request.host_url, f"{OPENSEARCH_ENDPOINT}/"),
-        headers         = {k:v for k,v in request.headers if k.lower() != "host"}, # exclude "host" header
-        data            = request.get_data(),
-        cookies         = request.cookies,
-        allow_redirects = False,
-    )
+    else:
+      # Based on https://stackoverflow.com/a/36601467
+      res = requests.request(
+          method          = request.method,
+          url             = request.url.replace(request.host_url, f"{OPENSEARCH_ENDPOINT}/"),
+          headers         = {k:v for k,v in request.headers if k.lower() != "host"}, # exclude "host" header
+          data            = request.get_data(),
+          cookies         = request.cookies,
+          allow_redirects = False,
+      )
+  
+      excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+  
+      headers = {}
+      for k,v in res.raw.headers.items():
+          if k not in excluded_headers:
+              headers[k] = v
+  
+      search_response = res.json()
+      
+      response = search_response
 
-    excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-
-    headers = {}
-    for k,v in res.raw.headers.items():
-        if k not in excluded_headers:
-            headers[k] = v
-
-    search_response = res.json()
-
-    for hit in search_response["hits"]["hits"]:
+      for hit in response["hits"]["hits"]:
         ean = hit["_source"]["ean"][0]
         cost = hit["_source"]["cost"]
+        
+        # Strip out of the sensitive data from what is sent to browser
         del hit["_source"]["cost"]
 
-        # Cache cost for products.
+        # Cache cost for product based on QueryId + EAN
         cache[ean] = cost
+  
+      response = flask.Response(json.dumps(search_response), res.status_code, headers=headers)
+  
+      return response
 
-    response = flask.Response(json.dumps(search_response), res.status_code, headers=headers)
+# This proxies the Multi Search _msearch end point across any index.
+# ecommerce/_msearch
+# ecommerce_keyword/_msearch
+# ecommerce_neural/_msearch
+@app.route("/<path:prefix>/_msearch", methods=["GET", "POST", "OPTIONS"])
+def multisearch(prefix):
 
-    return response
+    if request.method == "OPTIONS":
+        response = flask.jsonify(status=200, mimetype="application/json")
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Authorization,Content-Type")
+        response.headers.add("Access-Control-Request-Method", "*")
+        return response
+
+    else:
+      # Based on https://stackoverflow.com/a/36601467
+      res = requests.request(
+          method          = request.method,
+          url             = request.url.replace(request.host_url, f"{OPENSEARCH_ENDPOINT}/"),
+          headers         = {k:v for k,v in request.headers if k.lower() != "host"}, # exclude "host" header
+          data            = request.get_data(),
+          cookies         = request.cookies,
+          allow_redirects = False,
+      )
+  
+      excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
+  
+      headers = {}
+      for k,v in res.raw.headers.items():
+          if k not in excluded_headers:
+              headers[k] = v
+  
+      search_response = res.json()
+      
+      for response in search_response["responses"]:
+        for hit in response["hits"]["hits"]:
+          ean = hit["_source"]["ean"][0]
+          cost = hit["_source"]["cost"]
+          
+          # Strip out of the sensitive data from what is sent to browser
+          del hit["_source"]["cost"]
+  
+          # Cache cost for product based on QueryId + EAN
+          cache[ean] = cost
+  
+      response = flask.Response(json.dumps(search_response), res.status_code, headers=headers)
+  
+      return response
 
 
-@app.route("/ubi_events", methods=["POST", "OPTIONS"])
+@app.route("/ubi_events", methods=["PUT", "POST", "OPTIONS"])
 def ubi_events():
 
     if request.method == "OPTIONS":
@@ -75,6 +138,8 @@ def ubi_events():
 
         # Send the event to Data Prepper.
         events = request.get_json()
+        
+        print("I am posting an event")
 
         #print("Received UBI events:")
         #print(events)
@@ -119,35 +184,45 @@ def ubi_events():
         traceProvider.add_span_processor(processor)
         trace.set_tracer_provider(traceProvider)
 
-        trace.set_tracer_provider(TracerProvider(resource=resource))
         tracer = trace.get_tracer(__name__)
-        otlp_exporter = OTLPSpanExporter()
-
-        span_processor = BatchSpanProcessor(otlp_exporter)
-        trace.get_tracer_provider().add_span_processor(span_processor)
 
         for event in events:
+                     
+            # Add back the sensitive information (cost) to the data being sent to the UBI Events datastore.            
+            cost = None
+            
+            # couldn't get this to work so doing a more painful approach below.
+            # ean = event["event_attributes"]["object"]["object_id"] 
+            
+            event_attributes = event["event_attributes"]
+            if event_attributes is not None:
+              obj = event_attributes["object"]
+              if obj is not None:
+                ean = obj["object_id"]                    
+                if ean is not None:
+                  if ean in cache:
+                      print(f"Found EAN {ean} in cache")
+                      cost = cache[ean]
 
+            if cost is not None:
+              event['event_attributes']['cost'] = cost
+            
+            
+            # First we demonstrate indexing directly into ubi_events index
             client.index(
                 index="ubi_events",
                 body=event,
                 id=str(uuid.uuid4()),
                 refresh=True
             )
-
+              
+            # Now we demonstrate indexing via OTEL into otel_ubi_events index
             with tracer.start_as_current_span("ubi_event") as span:
 
                 for key, value in event.items():
-                    if value is not None and key is not "event_attributes":
+                    if value is not None and key != "event_attributes":
                         span.set_attribute("ubi." + key, value)
-
-                # TODO: Handle event_attributes
-
-                # Populate the cost (sensitive information) about the product.
-                ean = event["event_attributes"]["object"]["object_id"]
-                if ean in cache:
-                    cost = cache[ean]
-                    span.set_attribute("ubi.product_cost", cost)
+                span.set_attribute("ubi.event_attributes", json.dumps(event['event_attributes']))
 
         return '{"status": "submitted"}'
 
