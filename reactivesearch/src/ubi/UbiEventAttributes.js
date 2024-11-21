e67c83a49ada618c0cd3ec72cdab54dd48a72b4f
Eric Pugh <epugh@opensourceconnections.com>
2024-11-21 06:25:24 -0500

    Update to match what the spec expects

--- reactivesearch/src/ubi/UbiEventAttributes.js
+++ reactivesearch/src/ubi/UbiEventAttributes.js
@@ -1,11 +1,18 @@
 class UbiEventAttributes {
   constructor(idField, id = null, description = null, details = null) {
-    this.object_id = id;   
-    this.object_id_field = idField; 
-
-    // additional properties
-    this.description = description;   
-    this.object_detail = details;      
+    this.object = {
+      object_id: id,
+      object_id_field: idField,
+      description: description,
+      
+    }
+    
+    // merge in the details, but make sure to filter out any explicit properties
+    // since details is a free form that could be anything.  
+    var { object_id, object_id_field, description, ...filteredDetails } = details;
+    
+    this.object = { ...this.object, ...filteredDetails };
+   
   }
 }
 
