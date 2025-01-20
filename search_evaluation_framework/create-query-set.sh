c35da612bf56d0fb66b1032bafe635098449b218
jzonthemtn <jeff.zemerick@mtnfog.com>
2025-01-20 15:14:33 -0500

    Updating and removing scripts.
    Signed-off-by: jzonthemtn <jeff.zemerick@mtnfog.com>

--- search_evaluation_framework/create-query-set.sh
+++ search_evaluation_framework/create-query-set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # Create a query set using sampling.
-docker compose run search_evaluation_framework java -jar /app/search-evaluation-framework.jar -o http://opensearch:9200 -s creat-query-set.json
+docker compose run search_evaluation_framework java -jar /app/search-evaluation-framework.jar -o http://opensearch:9200 -s /app/files/create-query-set.json
