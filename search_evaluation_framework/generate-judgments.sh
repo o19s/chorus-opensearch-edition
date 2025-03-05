1e6d113078647c1fbd2f826713b5af1de55f1299
wrigleyDan <dwrigley@opensourceconnections.com>
2025-03-05 11:28:51 +0100

    changes for scripts and config files to align with latest search result quality app changes
    Signed-off-by: wrigleyDan <dwrigley@opensourceconnections.com>

--- search_evaluation_framework/generate-judgments.sh
+++ search_evaluation_framework/generate-judgments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # Create a click model.
-docker compose run search_evaluation_framework java -jar /app/search-evaluation-framework.jar -o http://opensearch:9200 -c coec
+docker compose run search_evaluation_framework java -jar /app/search-evaluation-framework.jar -o http://opensearch:9200 -c /app/files/judgments.json
