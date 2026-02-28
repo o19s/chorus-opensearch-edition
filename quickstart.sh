93c10d266240c85dbadf48d2b338a3b8f00662ea
Eric Pugh <epugh@opensourceconnections.com>
2026-02-28 09:29:13 -0500

    Be more consistent and use the long name

--- quickstart.sh
+++ quickstart.sh
@@ -103,7 +103,7 @@ do
 	shift
 done
 
-services="art opensearch-agent-server opensearch opensearch-dashboards middleware reactivesearch"
+services="agentic-relevance-tuning opensearch-agent-server opensearch opensearch-dashboards middleware reactivesearch"
 
 if $offline_lab; then
   services="${services} quepid"
